05566194b27fd02d862d53a205703790a2d30c7c
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-03-09 16:45:22 +0100

    Definitive Temperature calculations

--- temp_field_calc.m
+++ temp_field_calc.m
@@ -1,9 +1,11 @@
-function [T, Taux] = temp_field_calc(ap,ae, aw, bp, T, n)
+function [T, Taux] = temp_field_calc(ap,ae, aw, bp, T, n, Twall)
 % Here the iteration is executed.
 % The initial temperatures vector is stored in an auxiliar vector si the
 % error can be calculated to decide if the results are good or not.
 
 Taux = T;
+
+T(1) = Twall;
 
 for i = 2:n
     T(i) = (ae(i)*T(i+1) + aw(i)*T(i-1)+bp(i))/ap(i);
